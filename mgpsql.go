8fcf5a46b428b2adfbd51b61e4aded227d79ac1b
YslamB <berdalyyew99@gmail.com>
2024-09-01 15:29:14 +0500

    asdf

--- mgpsql.go
+++ mgpsql.go
@@ -7,8 +7,30 @@ import (
 	"log"
 	"time"
 
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+type Client interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Begin(ctx context.Context) (pgx.Tx, error)
+	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
+	Close()
+}
+
+type CommandTag pgconn.CommandTag
+
+type Options struct {
+	Host          string
+	Port          string
+	Database      string
+	Username      string
+	Password      string
+	PgPoolMaxConn int
+}
 
 func NewClient(ctx context.Context, options Options) (client Client, err error) {
 
